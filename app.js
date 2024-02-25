5ca7beb84122f3805ccdd4ec221be281863387cc
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2024-02-25 18:31:03 +0100

    feat: :hammer: initialized app & server files

---
+++ app.js
@@ -0,0 +1,34 @@
+// import dependencies
+import express from "express";
+import cors from "cors";
+
+// initialize app
+export const app = express();
+
+// CORS
+app.use(
+  cors({
+    origin: ["*"],
+    credentials: true,
+  })
+);
+
+// for parsing application/json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+// TODO: Add routes here
+
+// Error middleware
+app.use((err, req, res, next) => {
+  // Auth errors
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).json({ msg: "You do not have access to this page" });
+  }
+  if (err.name === "ErrorDocument") {
+    return res.status(404).json({ msg: "Page not found" });
+  }
+  // other errors to handle
+  return res.status(err.status).json({ error: err.message });
+});

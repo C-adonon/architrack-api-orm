9f475cc822e0f6a34bf3acc01bb91cd49247fec6
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2024-05-08 21:41:56 +0200

    feat: :goal_net: added zod validation & error middleware

---
+++ middlewares/zodMiddleware.js
@@ -0,0 +1,19 @@
+import { ZodError } from "zod";
+
+export function validateData(data) {
+  return (req, res, next) => {
+    try {
+      data.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue) => ({
+          message: `${issue.path.join(".")} is ${issue.message}`,
+        }));
+        res.status(400).json({ error: "Invalid data", details: errorMessages });
+      } else {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    }
+  };
+}

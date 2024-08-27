dd0dff83dfe26a8d9e7b8a17e1252eb459cdda54
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2024-08-28 01:52:17 +0200

    feat: :lock: added jwt middleware for auth

---
+++ auth/jwtMiddleware.js
@@ -0,0 +1,72 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+dotenv.config();
+
+function authenticateToken(req, res, next) {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json({ message: "Token not found" });
+
+  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: "Unauthorized" });
+    req.user = user;
+    next();
+  });
+}
+
+function generateTokens(user) {
+  const accessToken = jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_ACCESS_SECRET,
+    {
+      expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+    }
+  );
+  const refreshToken = jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_REFRESH_SECRET,
+    {
+      expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+    }
+  );
+  return { accessToken, refreshToken };
+}
+
+function generateAccessToken(user) {
+  return jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_ACCESS_SECRET,
+    {
+      expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+    }
+  );
+}
+
+// TODO FLAGS EN PRODUCTION : {  httpOnly: true,  secure: true,  sameSite: 'Strict' }
+// TODO FLAGS EN DEV : {  httpOnly: true,  secure: false,  sameSite: 'None/Lax' }
+function sendTokens(res, tokens) {
+  res.cookie("accessToken", tokens.accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "PROD" ? true : false,
+    sameSite: process.env.NODE_ENV === "PROD" ? "Strict" : "Lax",
+  });
+  res.cookie("refreshToken", tokens.refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "PROD" ? true : false,
+    sameSite: process.env.NODE_ENV === "PROD" ? "Strict" : "Lax",
+  });
+}
+
+function sendAccessToken(res, token) {
+  res.cookie("accessToken", token, {
+    httpOnly: true,
+    secure: false,
+  });
+}
+
+export {
+  generateTokens,
+  sendTokens,
+  authenticateToken,
+  generateAccessToken,
+  sendAccessToken,
+};

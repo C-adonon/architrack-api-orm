ab978c26c5b3735adb258b6e903a36b2403c7249
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2024-09-07 23:57:12 +0200

    feat: :sparkles: added uuid routes and replace id with uuid in existing routes

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,22 +5,34 @@ import {
   updateUser,
   deleteUser,
   getAllRoles,
+  getUserByUuid,
 } from "../controllers/userController.js";
 import { updatedUserValidator } from "../validators/userValidator.js";
 import { validateData } from "../middlewares/zodMiddleware.js";
 import { authenticateToken } from "../auth/jwtMiddleware.js";
 
 const router = express.Router();
+let uuidRegex =
+  "([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})";
+let idRegex = "([0-9]+)";
 
 router.get("/", authenticateToken, getAllUsers);
-router.get("/:id([0-9]+)", authenticateToken, getUserById);
+router.get("/:id" + idRegex, authenticateToken, getUserById);
+router.get("/:uuid" + uuidRegex, authenticateToken, getUserByUuid);
+router.get("/me", authenticateToken, getUserByUuid);
+router.put(
+  "/:uuid" + uuidRegex,
+  authenticateToken,
+  validateData(updatedUserValidator),
+  updateUser
+);
 router.put(
   "/:id([0-9]+)",
   authenticateToken,
   validateData(updatedUserValidator),
   updateUser
 );
-router.delete("/:id([0-9]+)", authenticateToken, deleteUser);
+router.delete("/:uuid" + uuidRegex, authenticateToken, deleteUser);
 router.get("/roles", authenticateToken, getAllRoles);
 
 export default router;

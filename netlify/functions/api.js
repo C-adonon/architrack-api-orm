bb839126e5cc801b3e097d9cce2ff395c0813218
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2025-07-20 04:28:31 +0200

    chore: :rocket: trying to deploy on netlify

---
+++ netlify/functions/api.js
@@ -0,0 +1,11 @@
+import express, { Router } from "express";
+import serverless from "serverless-http";
+
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/.netlify/functions/", router);
+
+export const handler = serverless(api);
